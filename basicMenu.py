dda9f1aa9cf38856ee84f5f146334e1f651fb042
khadada <lkhadada@gmail.com>
2022-04-02 22:27:16 +0200

    Creating initialize_ui method for myGUI

--- basicMenu.py
+++ basicMenu.py
@@ -3,3 +3,13 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QAction)
 
 # instanialted our class for the own GUI:
+class BasicMenu(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        
+    def initialize_ui(self):
+        """
+        Initialize the windaow and display its contents to the screen.
+        """
+        
